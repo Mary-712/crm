3dc73dec14688bfee60d716b4d1788f35faef5e0
Mary-712 <marywilsonc@gmail.com>
2024-04-04 18:03:01 +0530

    second commit

--- crmapp/urls.py
+++ crmapp/urls.py
@@ -7,6 +7,8 @@ urlpatterns = [
     path('login',views.login,name='login'),
     path('log',views.log,name='log'),
     path('new',views.new,name='new'),
+    path('dashboard',views.dashboard,name='dashboard'),
+    path('archieve',views.archieve,name='archieve'),
 
     
 ]+ static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
--- crmapp/views.py
+++ crmapp/views.py
@@ -9,7 +9,13 @@ def login(request):
     return render(request,'login.html')
 
 def log(request):
-    return redirect('home')
+    return redirect('dashboard')
 
 def new(request):
     return render(request,'new.html')
+
+def dashboard(request):
+    return render(request,'dashboard.html')
+def archieve(request):
+    item_ids = request.GET.get('item_ids',' ').split(',')
+    return render(request, 'Archieve.html', {'item_ids': item_ids})
